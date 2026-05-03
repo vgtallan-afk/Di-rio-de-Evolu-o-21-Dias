76d86031befb8ecf8a8dab260b13e867a5b7fdc4
vgtallan-afk <vgtallan@gmail.com>
2026-05-03 00:49:47 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'diario-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
